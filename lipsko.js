23b283629f20912b46411e4f93f7299ea92623c0
Vitaly Pavlenko <cxielamiko@gmail.com>
2016-02-12 11:41:54 +0300

    Fix alignment editor

--- lipsko.js
+++ lipsko.js
@@ -10,26 +10,44 @@ $(function() {
     var id_counter = 0;
 
     function wrap_to_spans_and_insert($demo, $editor) {
-        var lines = $demo.text().split("\n");
+        var lines = $demo.text().trim().split("\n");
         var editor_lines = [];
         for (var line of lines) {
             var editor_line = [];
             for (var word of line.split(" ")) {
-                editor_line.push('<span class="lipsko-word">' + word + '</span>');
+                if (word) {
+                    editor_line.push('<span class="lipsko-word">' + word + '</span>');
+                }
             }
             editor_lines.push(editor_line.join(' '));
         }
         $editor.html(editor_lines.join("\n"));
     }
 
+    function update_markup() {
+        $('#lipsko-editor .lipsko-source-markup').val($('#lipsko-editor .lipsko-source').html());
+        $('#lipsko-editor .lipsko-translation-markup').val($('#lipsko-editor .lipsko-translation').html());
+    }
+
     $('#lipsko-editor-start').click(function() {
         wrap_to_spans_and_insert($('#lipsko-demo .lipsko-source'), $('#lipsko-editor .lipsko-source'));
         wrap_to_spans_and_insert($('#lipsko-demo .lipsko-translation'), $('#lipsko-editor .lipsko-translation'));
+        update_markup();
+    });
+
+    $('#lipsko-editor-copy').click(function() {
+        $('#lipsko-editor .lipsko-source').html($('#lipsko-demo .lipsko-source').html());
+        $('#lipsko-editor .lipsko-translation').html($('#lipsko-demo .lipsko-translation').html());
+        update_markup();
     });
 
     $(document).on('click', '#lipsko-editor .lipsko-word', function(event) {
         var $target = $(event.target);
-        if (!event.shiftKey && $target.parent('.lipsko-source').length) {
+        if ($target.hasClass('lipsko-editor-word-editing')) {
+            $target.removeClass('lipsko-editor-word-editing')
+                .removeClass('lipsko-highlight')
+                .removeAttr('lipsko-id');
+        } else if (!event.shiftKey && $target.parent('.lipsko-source').length) {
             $('.lipsko-editor-word-editing').removeClass('lipsko-editor-word-editing');
             $target.addClass('lipsko-editor-word-editing');
             $target.attr('lipsko-id', ++id_counter);
@@ -37,10 +55,7 @@ $(function() {
             $target.addClass('lipsko-editor-word-editing');
             $target.attr('lipsko-id', id_counter);
         }
-    });
 
-    $('#lipsko-editor-apply').click(function() {
-        $('#lipsko-demo .lipsko-source').html($('#lipsko-editor .lipsko-source').html());
-        $('#lipsko-demo .lipsko-translation').html($('#lipsko-editor .lipsko-translation').html());
+        update_markup();
     });
 });
